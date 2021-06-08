6ef8267ba91b341dbe823d9e4ab3df03afc4cc98
Sahand Hamzehei <73257250+SahandHamzehei@users.noreply.github.com>
2021-06-08 14:15:57 +0200

    Update manpop_story.js

--- scripts/manpop_story.js
+++ scripts/manpop_story.js
@@ -8,8 +8,10 @@ var storyContent = d3.select("#storymode-content p");
 
 // STORIES //
 var stories = [
-  { title: "The Story of CNS disease and PM value in Italy.",
-    description: "The visualizations of the dashboard try to show the connection between CNS disease and PM values in Italy. It is well known that dust has harmful effects that can lead to a variety of diseases. The majority of evidence on the connection between dust and health has focused on lung and cardiovascular diseases. The effects of air pollution on the central nervous system (CNS), on the other hand, are not well-founded. Each story talks about the summarization of the pollution or CNS for one or more regions to give useful data.",
+
+  { title: "The Visualization of Central Nervous System disease and PM2.5 in Italy.",
+    description: "This dashboard shows the connection between CNS disease and PM values in Italy. It is known from research that pollution has harmful effects that can lead to a variety of diseases. The majority of evidence on the connection between dust and health has focused on lung and cardiovascular diseases. The effects of air pollution on the central nervous system (CNS), on the other hand, are a more recent discovery. \
+    Each of the following stories, supports the visualization that shows the most relevant, or curious statistics about the distribution of the pollution level and central nervous system diseases in italian provinces.",
     districts: [01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20],
     day: 0,
     time: 0,
@@ -18,154 +20,151 @@ var stories = [
       zoom: 3.50,
       bearing: -20,
       pitch: 45.00,
-      speed: 0.3,
+      speed: 0.3
     },
     flyToMobile: {
       center: [12.5674,41.8719],
       zoom: 0,
       bearing: -20,
       pitch: 45.00,
-      speed: 0.1
+      speed: 0.3
     }
   },
 
 // Story 2
-  { title: "Min and max values of CNS.",
-    description: "Regarding the dataset, Campania and Calabria are two countries with the lowest rate of CNS disease for all three years. On the other hand, the maximum value of the CNS disease is for Liguria for 2015 and 2016, and Marcas for 2017.",
-    districts: [18,15,07,11],
-    day: 0,
-    time: 0,
-    flyTo: {
-      center: [12.5674,41.8719],
-      zoom: 5.6,
-      bearing: -20,
-      pitch: 55.00,
-      speed: 0.3
-    },
-    flyToMobile: {
-      center: [12.5674,41.8719],
-      zoom: 0,
-      bearing: -20,
-      pitch: 75.00,
-      speed: 0.3
-    }
-  },
+{ title: "Most and least provinces for values of Central Nervous System Deaths",
+description: "Campania and Calabria are two countries with the lowest rate of CNS disease deaths for all the three years considered. On the other hand, the maximum value of the CNS disease caused deaths is for Liguria for 2015 and 2016, and Marche for 2017.",
+districts: [18,15,07,11],
+day: 0,
+time: 0,
+flyTo: {
+  center: [12.5674,41.8719],
+  zoom: 5.6,
+  bearing: -20,
+  pitch: 55.00,
+  speed: 0.3
+},
+flyToMobile: {
+  center: [12.5674,41.8719],
+  zoom: 0,
+  bearing: -20,
+  pitch: 55.00,
+  speed: 0.3
+}
+},
 
-  // Story 3
-  { title: "The most polluted and cleanest regions in Italy.",
-  description: "From the dataset, it can be seen that during three years, Lombardia is the most polluted region in Italy (in the case of pm2.5) In 2017, it has the most pollution level. Regarding the research, The Lombardia region in northern Italy ranks among the most air polluted areas of Europe. By considering the min values of PM2.5, Valle d'Aosta is the region that for all three years has the min values.",
-  districts: [03,02],
-  day: 1,
-  time: 2,
-  flyTo: {
-    center: [8.51,46.00],
-    zoom: 6.50,
-    bearing: -20,
-    pitch: 65.00,
-    speed: 0.3
-  },
-  flyToMobile: {
-    center: [12.5674,41.8719],
-    zoom: 5.0,
-    bearing: -20,
-    pitch: 45.00,
-    speed: 0.3
-    }
-  },
+ // Story 3
+ { title: "The most polluted and cleanest regions in Italy.",
+ description: "Lombardia is the most polluted region in Italy (in the case of pm2.5) and in 2017, it has the most pollution level. Lombardia furthermore ranks among the most air polluted areas of Europe. By considering the min values of PM2.5, Valle d'Aosta is the region that for all the considered three years has the min values.",
+ districts: [03,02],
+ day: 1,
+ time: 2,
+ flyTo: {
+   center: [8.51,46.00],
+   zoom: 6.50,
+   bearing: -20,
+   pitch: 65.00,
+   speed: 0.3
+ },
+ flyToMobile: {
+   center: [8.51,46.00],
+   zoom: 1.0,
+   bearing: -20,
+   pitch: 65.00,
+   speed: 0.3
+ }
+},
 
+ // Story 4
+ { title: "Piemonte",
+ description: "Piemonte is one of the primary regions in Italy. In the case of PM2.5 for all the considered three years, it is the third polluted region in Italy and in 2017, Piemonte has been the most polluted region in italy",
+ districts: [01],
+ day: 1,
+ time: 2,
+ flyTo: {
+   center: [7.51,45.05],
+   zoom: 6.50,
+   bearing: -20,
+   pitch: 75.00,
+   speed: 0.3
+ },
+ flyToMobile: {
+   center: [7.51,45.05],
+   zoom: 1.50,
+   bearing: -20,
+   pitch: 75.00,
+   speed: 0.3
+ }
+},
 
-  // Story 4
-  { title: "Piemonte",
-  description: "Piemonte is one of the primary regions in Italy. In the case of PM2.5 for all three years, it is the third polluted region in Italy. Also in 2017, piemonte experienced the most pollution",
-  districts: [01],
-  day: 1,
-  time: 2,
-  flyTo: {
-    center: [7.51,45.05],
-    zoom: 6.50,
-    bearing: -20,
-    pitch: 75.00,
-    speed: 0.3
-  },
-  flyToMobile: {
-    center: [12.5674,41.8719],
-    zoom: 2,
-    bearing: -20,
-    pitch: 75.00,
-    speed: 0.3
-    }
-  },
-
-  // Story 5
-  { title: "CNS in most polluted and cleanest",
-  description: "As mentioned, Lombardia is the most polluted region in Italy. However among all regions, in the case of CNS disease, it is located in the mid of the table, 8th for 2015, 7th for 2016, and 10th for 2017 (from largest values to smallest). In contrast, the Valle d'Aosta is the cleanest city and in the case of CNS, it is located in 14th in 2015, 15th in 2016, and 9th in 2017.",
-  districts: [03,02],
-  day: 0,
-  time: 0,
-  flyTo: {
-    center: [8.51,46.00],
-    zoom: 6.50,
-    bearing: -20,
-    pitch: 75.00,
-    speed: 0.3
-  },
-  flyToMobile: {
-    center: [12.5674,41.8719],
-    zoom: 1.50,
-    bearing: -20,
-    pitch: 45.00,
-    speed: 0.3
-    }
-  },
+ // Story 5
+ { title: "CNS in most polluted and cleanest",
+ description: "As mentioned, Lombardia is the most polluted region in Italy. However among all regions, in the case of CNS disease caused deaths, relative to the population, it is located in the middle of the ranking, 8th for 2015, 7th for 2016, and 10th for 2017 (from largest values to smallest). In contrast, Valle d'Aosta is the cleanest region and in the case of CNS, it is located in 14th in 2015, 15th in 2016, and 9th in 2017.",
+ districts: [03,02],
+ day: 0,
+ time: 0,
+ flyTo: {
+   center: [8.51,46.00],
+   zoom: 6.50,
+   bearing: -20,
+   pitch: 75.00,
+   speed: 0.3
+ },
+ flyToMobile: {
+   center: [8.51,46.00],
+   zoom: 1.50,
+   bearing: -20,
+   pitch: 75.00,
+   speed: 0.3
+ }
+},
 
 
  // Story 6
-  { title: "Capital of Italy",
-  description: "Rome, the capital of Italy is located in the Lacio region. During the three years, it has the constant values in the CNS. However, in 2015 it is the 7th polluted region in Italy. The point is that in 2017, the pollution decrease, and Lacio located in the mid of the table of PM2.5 value. It has the less pollution in 2016",
-  districts: [12],
-  day: 1,
-  time: 1,
-  flyTo: {
-    center: [12.98,41.65],
-    zoom: 6.50,
-    bearing: -20,
-    pitch: 75.00,
-    speed: 0.3
-  },
-  flyToMobile: {
-    center: [12.5674,41.8719],
-    zoom: 0,
-    bearing: -20,
-    pitch: 75.00,
-    peed: 0.3
-    }
-  },
+ { title: "Capital of Italy",
+ description: "Rome, the capital of Italy is located in the Lazio region. During the considered three years, it has almost constant values in the CNS caused mortality rate.",
+ districts: [12],
+ day: 1,
+ time: 1,
+ flyTo: {
+   center: [12.98,41.65],
+   zoom: 6.50,
+   bearing: -20,
+   pitch: 75.00,
+   speed: 0.3
+ },
+ flyToMobile: {
+   center: [12.98,41.65],
+   zoom: 1.50,
+   bearing: -20,
+   pitch: 75.00,
+   peed: 0.3
+ }
+},
 
  // Story 7
-  { title: "Constancy during three years",
-  description: "In the case of the constancy during three years in PM values, Veneto and Basilicata have the most stability in PM2.5. But on the other hand, Marche has the most fluctuation.",
-  districts: [05,17,11],
-  day: 1,
-  time: 0,
-  flyTo: {
-    center: [12.5674,41.8719],
-    zoom: 5.50,
-    bearing: -20,
-    pitch: 45.00,
-    speed: 0.3
-  },
-  flyToMobile: {
-    center: [12.5674,41.8719],
-    zoom: 0,
-    bearing: -20,
-    pitch: 45.00,
-    speed: 0.3
-    }
-  },
+ { title: "Constancy during the considered years",
+ description: "Veneto and Basilicata have the most stability in PM2.5 and on the other hand, Marche has the most higher fluctuation.",
+ districts: [05,17,11],
+ day: 1,
+ time: 0,
+ flyTo: {
+   center: [12.5674,41.8719],
+   zoom: 4.50,
+   bearing: -20,
+   pitch: 45.00,
+   speed: 0.3
+ },
+ flyToMobile: {
+  center: [12.5674,41.8719],
+  zoom: 1.50,
+  bearing: -20,
+  pitch: 45.00,
+  speed: 0.3
+ }
+},
   
 ];
-
-
 
 ///////////// FUNCTIONS ///////////////
 
